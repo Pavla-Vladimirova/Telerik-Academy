b1072bc97ee8698f493ca0407dcacc860b5f954f
Pavla Vladimirova <pavla.vladimirova@gmail.com>
2013-03-03 16:20:16 +0200

    Update CheckPointInACircleAndOutOfRectangle.cs

--- Programming Track/CSharpPartOne/03.OperatorsExpressionsAndStatements/9.CheckPointInACircleAndOutRectangle/CheckPointInACircleAndOutOfRectangle.cs
+++ Programming Track/CSharpPartOne/03.OperatorsExpressionsAndStatements/9.CheckPointInACircleAndOutRectangle/CheckPointInACircleAndOutOfRectangle.cs
@@ -14,12 +14,14 @@ namespace _9.CheckPointInACircleAndOutRectangle
             sbyte x = sbyte.Parse(Console.ReadLine());
             Console.WriteLine("Write the \"y\" cordinate of your pont");
             sbyte y = sbyte.Parse(Console.ReadLine());
-            bool inCircle = ((Math.Sqrt(x * x + y * y)) <= 5);
-            Console.WriteLine(inCircle ? "Your point is in the circle" : "Your point is out of the circle");
-            bool outOfRectangle=((x<=-1)||(x>=5))&&((y<=1)||(y>=5));
-            Console.WriteLine(outOfRectangle ? "Your point is out of the rectangle" : "Your point is in  the rectangle");
-            bool inAndOut=(inCircle&&outOfRectangle);
-            Console.WriteLine(inAndOut ? "Your point is at the same time IN the circle and OUT of the rectangle" : "Your point IS NOT at the same time in the circle and OUT of the rectangle");
+            bool inCircle =(Math.Sqrt((x - 1) * (x - 1) + (y - 1) * (y - 1))) <= 3;
+            Console.WriteLine(inCircle ? "Your point is in the circle K( (1,1), 3)" : "Your point is out of the circle K( (1,1), 3)");
+
+            bool inRectangle = ((x <= 7) && (x >= 1)) && ((y <= -1) && (y >= -3));
+            Console.WriteLine(inRectangle ? "Your point is in  the rectangle R(top=1, left=-1, width=6, height=2)" : "Your point is out of the rectangle R(top=1, left=-1, width=6, height=2)");
+            
+            bool inAndOut=((inCircle==true)&&(inRectangle==false));
+            Console.WriteLine(inAndOut ? "Your point is at the same time IN the circle K( (1,1), 3) and OUT of the rectangle R(top=1, left=-1, width=6, height=2)" : "Your point IS NOT at the same time in the circle K( (1,1), 3) and OUT of the rectangle R(top=1, left=-1, width=6, height=2)");
         }
     }
 }
